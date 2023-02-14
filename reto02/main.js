aebb9e54f5eec56e7dd5aaee2da68f931da4878b
Matias <m.araujoge@gmail.com>
2023-02-14 20:26:41 -0300

    Agregando reto02

---
+++ reto02/main.js
@@ -0,0 +1,14 @@
+//chocolate menu
+let btnMenu = document.getElementById('btnMenu')
+let menuChoc = document.getElementById('menuChoc')
+
+btnMenu.addEventListener('click', () => {
+    if(menuChoc.style.display = 'none'){
+        menuChoc.style.display = 'block';
+    } else{
+        menuChoc.style.display = 'none'
+    }
+    
+})
+
+
