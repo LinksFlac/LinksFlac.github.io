472357827e804bb80a1cb919533f968d00ac04c2
Matias <m.araujoge@gmail.com>
2023-02-14 23:36:16 -0300

    Aditamos el funcionamiento  de chocolate boton

--- reto02/main.js
+++ reto02/main.js
@@ -3,11 +3,7 @@ let btnMenu = document.getElementById('btnMenu')
 let menuChoc = document.getElementById('menuChoc')
 
 btnMenu.addEventListener('click', () => {
-    if(menuChoc.style.display = 'none'){
-        menuChoc.style.display = 'block';
-    } else{
-        menuChoc.style.display = 'none'
-    }
+    menuChoc.classList.toggle('active')
     
 })
 
